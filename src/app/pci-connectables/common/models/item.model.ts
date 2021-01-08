0b0b6c52e71109991aed1f0e408a67b542e36356
pyramidsoftwaresolutions <53467574+pyramidsoftwaresolutions@users.noreply.github.com>
2021-01-09 00:46:20 +0530

    Update item.model.ts

--- src/app/pci-connectables/common/models/item.model.ts
+++ src/app/pci-connectables/common/models/item.model.ts
@@ -1,42 +1,50 @@
 /**to be used both externally and internall to pass input data to the framework */
-export class ConnectableItemModel{
-    data:any;
-    displayText?:string;
-    displayDataField?:string;
-    __isSelected?:boolean;
-    color?:string;
+export class ConnectableItemModel {
+  data: any;
+  displayText?: string;
+  displayDataField?: string;
+  __isSelected?: boolean;
+  __isMapped?: boolean;
+  color?: string;
 }
 
 /**to be used internally */
-export class PairItemModel{
-    item:ConnectableItemModel;
-    element:HTMLElement;
+export class PairItemModel {
+  item: ConnectableItemModel;
+  element: HTMLElement;
 
-    constructor(item:ConnectableItemModel, element:HTMLElement){
-        this.item=item;
-        this.element=element;
-    }
+  constructor(item: ConnectableItemModel, element: HTMLElement) {
+    this.item = item;
+    this.element = element;
+  }
 }
 
 /**can be used externally to get or set mapped data items */
-export class MappedSourceTargetItem{
-    source:ConnectableItemModel;
-    target:ConnectableItemModel;
-    constructor(source:ConnectableItemModel, target:ConnectableItemModel){
-        this.source=source;
-        this.target=target;
-    }
+export class MappedSourceTargetItem {
+  source: ConnectableItemModel;
+  target: ConnectableItemModel;
+  constructor(source: ConnectableItemModel, target: ConnectableItemModel) {
+    this.source = source;
+    this.target = target;
+  }
 }
 
 /**internally used for mapping with line connector along with source and target */
-export class MappedLineAndPairItem{
-    line: any;
-    source:PairItemModel;
-    target:PairItemModel;
+export class MappedLineAndPairItem {
+  line: any;
+  source: PairItemModel;
+  target: PairItemModel;
+  lineId: string;
 
-    constructor(line:any, source:PairItemModel, target:PairItemModel){
-        this.line=line;
-        this.source=source;
-        this.target=target;
-    }
+  constructor(
+    line: any,
+    source: PairItemModel,
+    target: PairItemModel,
+    lineId: string
+  ) {
+    this.line = line;
+    this.source = source;
+    this.target = target;
+    this.lineId = lineId;
+  }
 }
