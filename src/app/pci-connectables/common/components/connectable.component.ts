a8b8880c8ff41464c5315ddeae3e1f29d18d7a8a
pyramidsoftwaresolutions <53467574+pyramidsoftwaresolutions@users.noreply.github.com>
2021-01-09 00:48:29 +0530

    Update connectable.component.ts

--- src/app/pci-connectables/common/components/connectable.component.ts
+++ src/app/pci-connectables/common/components/connectable.component.ts
@@ -1,70 +1,91 @@
-import { Component, OnInit, Input, Output, EventEmitter, OnChanges, ViewChild, ElementRef, OnDestroy } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  Output,
+  EventEmitter,
+  OnChanges,
+  ViewChild,
+  ElementRef,
+  OnDestroy,
+} from '@angular/core';
 import { ConnectableItemModel, PairItemModel } from '../models/item.model';
-declare var PlainDraggable:any;
-declare var traceLog:any;
+declare var PlainDraggable: any;
+declare var traceLog: any;
 
 @Component({
   selector: 'pci-connectable',
   templateUrl: 'connectable.component.html',
-  styleUrls: ['connectable.component.scss']
+  styleUrls: ['connectable.component.scss'],
 })
 export class ConnectableComponent implements OnInit, OnChanges, OnDestroy {
-  @Input() item:ConnectableItemModel;
-  @Input() draggable:boolean=true;
-  
-  @ViewChild('draggable') containerElement:ElementRef;
-  @Output() rendered:EventEmitter<PairItemModel> = new EventEmitter<PairItemModel>();
-  @Output() dragging:EventEmitter<any> = new EventEmitter<any>();
-  @Output() dragend:EventEmitter<any> = new EventEmitter<any>();
-  @Output() selected:EventEmitter<any> = new EventEmitter<any>();
-  draggableEle:any;
-  dragData:{started?:boolean, ended?:boolean, startPosition?:any, endPosition?:any, movingPosition?:any}={};
+  @Input() item: ConnectableItemModel;
+  @Input() draggable: boolean = true;
+  @Input() showRemove: boolean = true;
 
-  constructor() { 
+  @ViewChild('draggable') containerElement: ElementRef;
+  @Output()
+  rendered: EventEmitter<PairItemModel> = new EventEmitter<PairItemModel>();
+  @Output() dragging: EventEmitter<any> = new EventEmitter<any>();
+  @Output() dragend: EventEmitter<any> = new EventEmitter<any>();
+  @Output() selected: EventEmitter<any> = new EventEmitter<any>();
+  @Output() remove: EventEmitter<any> = new EventEmitter<any>();
+  draggableEle: any;
+  dragData: {
+    started?: boolean;
+    ended?: boolean;
+    startPosition?: any;
+    endPosition?: any;
+    movingPosition?: any;
+  } = {};
 
-  }
+  constructor() {}
 
-  ngOnInit(): void {
-    
-  }
+  ngOnInit(): void {}
 
   ngAfterViewInit() {
-    var ____this=this;
-    if(this.draggable){
-      this.draggableEle = new PlainDraggable(this.containerElement.nativeElement);
-      this.draggableEle.onDrag = function(newPosition) {
+    var ____this = this;
+    if (this.draggable) {
+      this.draggableEle = new PlainDraggable(
+        this.containerElement.nativeElement
+      );
+      this.draggableEle.onDrag = function (newPosition) {
         //traceLog.add(newPosition);
-        ____this.dragData.started=true;
-        ____this.dragData.ended=false;
-        ____this.dragData.startPosition=newPosition;
-        ____this.dragData.endPosition=undefined;
+        ____this.dragData.started = true;
+        ____this.dragData.ended = false;
+        ____this.dragData.startPosition = newPosition;
+        ____this.dragData.endPosition = undefined;
       };
-      this.draggableEle.onMove = function(newPosition) {
+      this.draggableEle.onMove = function (newPosition) {
         traceLog.add(newPosition);
-        ____this.dragData.movingPosition=newPosition;
+        ____this.dragData.movingPosition = newPosition;
         ____this.dragging.emit(____this.dragData);
       };
-      this.draggableEle.onDragEnd = function(newPosition) {
+      this.draggableEle.onDragEnd = function (newPosition) {
         //traceLog.add(newPosition);
-        ____this.dragData.ended=true;
-        ____this.dragData.endPosition=newPosition;
+        ____this.dragData.ended = true;
+        ____this.dragData.endPosition = newPosition;
         ____this.dragend.emit(____this.dragData);
       };
     }
     let arg = new PairItemModel(this.item, this.containerElement.nativeElement);
     this.rendered.emit(arg);
   }
-  ngOnChanges(){
-  }
+  ngOnChanges() {}
 
-  ngOnDestroy(){
-    if(this.draggableEle){
+  ngOnDestroy() {
+    if (this.draggableEle) {
       this.draggableEle.remove();
     }
   }
 
-  onItemSelected(evt:any){
+  onItemSelected(evt: any) {
     let arg = new PairItemModel(this.item, this.containerElement.nativeElement);
     this.selected.emit(arg);
+  }
+
+  removeLink(item: any) {
+    let arg = new PairItemModel(this.item, this.containerElement.nativeElement);
+    this.remove.emit(arg);
   }
 }
