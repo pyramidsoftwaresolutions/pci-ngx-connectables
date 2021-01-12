53a106373d943999ed8ff7887c902d5f41d7c787
pyramidsoftwaresolutions <53467574+pyramidsoftwaresolutions@users.noreply.github.com>
2021-01-12 12:32:51 +0530

    Update connectable.component.ts

--- src/app/pci-connectables/common/components/connectable.component.ts
+++ src/app/pci-connectables/common/components/connectable.component.ts
@@ -26,57 +26,57 @@ export class ConnectableComponent implements OnInit, OnChanges, OnDestroy {
   @ViewChild('draggable') containerElement: ElementRef;
   @Output()
   rendered: EventEmitter<PairItemModel> = new EventEmitter<PairItemModel>();
-  @Output() dragging: EventEmitter<any> = new EventEmitter<any>();
-  @Output() dragend: EventEmitter<any> = new EventEmitter<any>();
+  // @Output() dragging: EventEmitter<any> = new EventEmitter<any>();
+  // @Output() dragend: EventEmitter<any> = new EventEmitter<any>();
   @Output() selected: EventEmitter<any> = new EventEmitter<any>();
   @Output() remove: EventEmitter<any> = new EventEmitter<any>();
-  draggableEle: any;
-  dragData: {
-    started?: boolean;
-    ended?: boolean;
-    startPosition?: any;
-    endPosition?: any;
-    movingPosition?: any;
-  } = {};
+  //draggableEle: any;
+  // dragData: {
+  //   started?: boolean;
+  //   ended?: boolean;
+  //   startPosition?: any;
+  //   endPosition?: any;
+  //   movingPosition?: any;
+  // } = {};
 
   constructor() {}
 
   ngOnInit(): void {}
 
   ngAfterViewInit() {
-    var ____this = this;
-    if (this.draggable) {
-      this.draggableEle = new PlainDraggable(
-        this.containerElement.nativeElement
-      );
-      this.draggableEle.onDrag = function (newPosition) {
-        //traceLog.add(newPosition);
-        ____this.dragData.started = true;
-        ____this.dragData.ended = false;
-        ____this.dragData.startPosition = newPosition;
-        ____this.dragData.endPosition = undefined;
-      };
-      this.draggableEle.onMove = function (newPosition) {
-        traceLog.add(newPosition);
-        ____this.dragData.movingPosition = newPosition;
-        ____this.dragging.emit(____this.dragData);
-      };
-      this.draggableEle.onDragEnd = function (newPosition) {
-        //traceLog.add(newPosition);
-        ____this.dragData.ended = true;
-        ____this.dragData.endPosition = newPosition;
-        ____this.dragend.emit(____this.dragData);
-      };
-    }
+    // var ____this = this;
+    // if (this.draggable) {
+    //   this.draggableEle = new PlainDraggable(
+    //     this.containerElement.nativeElement
+    //   );
+    //   this.draggableEle.onDrag = function (newPosition) {
+    //     //traceLog.add(newPosition);
+    //     ____this.dragData.started = true;
+    //     ____this.dragData.ended = false;
+    //     ____this.dragData.startPosition = newPosition;
+    //     ____this.dragData.endPosition = undefined;
+    //   };
+    //   this.draggableEle.onMove = function (newPosition) {
+    //     traceLog.add(newPosition);
+    //     ____this.dragData.movingPosition = newPosition;
+    //     ____this.dragging.emit(____this.dragData);
+    //   };
+    //   this.draggableEle.onDragEnd = function (newPosition) {
+    //     //traceLog.add(newPosition);
+    //     ____this.dragData.ended = true;
+    //     ____this.dragData.endPosition = newPosition;
+    //     ____this.dragend.emit(____this.dragData);
+    //   };
+    // }
     let arg = new PairItemModel(this.item, this.containerElement.nativeElement);
     this.rendered.emit(arg);
   }
   ngOnChanges() {}
 
   ngOnDestroy() {
-    if (this.draggableEle) {
-      this.draggableEle.remove();
-    }
+    // if (this.draggableEle) {
+    //   this.draggableEle.remove();
+    // }
   }
 
   onItemSelected(evt: any) {
